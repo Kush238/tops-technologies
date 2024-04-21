d040a8d9640d8497c1c76a26c9c996cbe5d19927
Kush Brahmbhatt <kushbrahmbhatt23@gmail.com>
2024-04-21 22:48:08 +0530

    Done Que 1 and Que 2
    Done Que 1 and Que 2

--- software-engineering/module-3/fundamentals-of-programming/conditional-logic-programs/que-1.c
+++ software-engineering/module-3/fundamentals-of-programming/conditional-logic-programs/que-1.c
@@ -3,12 +3,23 @@
 #include <stdio.h>
 
 main() {
-	
-	int num1, num2;
-	
-	printf("Enter Number 1 : ");
-	scanf("%f", &num1);
-	
-	printf("\nEnter Number 2 : ");
-	scanf("%f", &num2);
+
+  int num1, num2;
+
+  printf("Enter Number 1 : ");
+  scanf("%d", &num1);
+
+  printf("\nEnter Number 2 : ");
+  scanf("%d", &num2);
+
+  if (num1 == num2) {
+    printf("\nEQUAL!!!");
+    printf("\nNumber 1: %d and Number 2 %d are equal", num1, num2);
+  } else if (num1 > num2) {
+    printf("\nNOT EQUAL!!!");
+    printf("\nNumber 1: %d is greater than Number 2 %d", num1, num2);
+  } else {
+    printf("\nNOT EQUAL!!!");
+    printf("\nNumber 2: %d is greater than Number 1 %d", num2, num1);
+  }
 }
