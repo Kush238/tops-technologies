3270c78485893ada3b6ef3af3a274353b1bcf4b6
Kush Brahmbhatt <kushbrahmbhatt23@gmail.com>
2024-04-21 21:26:48 +0530

    Que -1 half Done
    Que -1 half Done

---
+++ software-engineering/module-3/fundamentals-of-programming/conditional-logic-programs/que-1.c
@@ -0,0 +1,14 @@
+// Write a C program to accept two integers and check whether they are equal or not
+
+#include <stdio.h>
+
+main() {
+	
+	int num1, num2;
+	
+	printf("Enter Number 1 : ");
+	scanf("%f", &num1);
+	
+	printf("\nEnter Number 2 : ");
+	scanf("%f", &num2);
+}
